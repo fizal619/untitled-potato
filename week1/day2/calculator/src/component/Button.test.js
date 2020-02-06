e9666fa4844ce72962ae9ecab4d50ca1dceb5cc9
Fizal Sarif <fizal.sarif@gmail.com>
2020-02-06 11:51:45 -0500

    test the mock function

--- week1/day2/calculator/src/component/Button.test.js
+++ week1/day2/calculator/src/component/Button.test.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "./Button";
-import { act } from "react-dom/test-utils";
-import {fireEvent, screen, getByText} from '@testing-library/react';
 
 it("Button has correct classes by default", () => {
   const div = document.createElement("div");
@@ -34,3 +32,30 @@ it("Button has correct classes with props", () => {
 
 });
 
+// We did not need enzyme for the previous tests.
+// Since this test does some more advanced stuff - we need enzyme
+// npm i -D enzyme enzyme-adapter-react-16
+import { shallow, configure,  } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() })
+
+it("Button click handler fires correctly", () => {
+  // we use the jest placeholder function
+  const handlerFn = jest.fn();
+
+  // shallow render the component,
+  // so children components won't be rendered (fast)
+  const btn = shallow(<Button
+    name="TEST"
+    clickHandler={handlerFn}
+  />);
+
+  // btn is actually a div,
+  // click the actual button el inside of it
+  btn.find('button').simulate('click');
+
+  // expect the placeholder function to have been called
+  expect(handlerFn).toHaveBeenCalled();
+
+});
